50947a4cc109ea006d886a70a092b5bc568732b8
Shay+1-at-727768565934 <ebert.shay@gmail.com>
2025-06-01 22:19:10 -0500

    dumb shit finished

--- app.config.js
+++ app.config.js
@@ -7,7 +7,15 @@ export default {
         extra: {
             SUPABASE_URL: process.env.SUPABASE_URL,
             SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
+            eas: {
+                projectId: "9803f32a-19ab-419b-bf0e-e3a91f7043aa"
+            }
         },
-        "backgroundColor": "#FAFAFA"
+        owner: 'shayebertowski',
+        backgroundColor: "#FAFAFA",
+        updates: {
+            url: "https://u.expo.dev/9803f32a-19ab-419b-bf0e-e3a91f7043aa"
+        },
+        runtimeVersion: "1.0.0"
     },
 };
